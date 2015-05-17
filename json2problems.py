ac0b8fac9b8fb3753b830c9e57dd6db09875a14d
Dmitry Sutygin <demonscorpion@yandex.ru>
2015-05-17 13:05:31 +0400

    added serialization and deserialization of problems and tags

---
+++ json2problems.py
@@ -0,0 +1,30 @@
+import logging
+from problem import Problem, Solution
+import json
+
+
+def deserialize_problems(fp):
+    problems, tags = [], []
+
+    try:
+        default_obj = json.load(fp=fp)
+        for problem_and_tags in default_obj:
+            tmp_problem = problem_and_tags[0]
+            tmp_tags = problem_and_tags[1]
+
+            problem = Problem(statement=tmp_problem["statement"], link=tmp_problem["link"])
+            problem.solutions = [Solution(solution["code"], solution["language"])
+                                 for solution in tmp_problem["solutions"]]
+
+            problems.append(problem)
+            tags.append(tmp_tags)
+
+    except (TypeError, KeyError) as e:
+        logging.exception(e)
+
+    return problems, tags
+
+
+def deserialize_problems_from_file(path_to_file):
+    with open(path_to_file, "r") as f:
+        return deserialize_problems(f)
---
+++ problems2json.py
@@ -0,0 +1,21 @@
+import json
+from problem import Problem, Solution
+
+
+class ProblemEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Problem):
+            return {"statement": o.statement, "link": o.link, "solutions": o.solutions}
+        if isinstance(o, Solution):
+            return {"language": o.language, "code": o.code}
+        return json.JSONEncoder.default(self, o)
+
+
+def serialize_problems(problems, tags, fp):
+    to_serialize = [(problem, tag) for problem, tag in zip(problems, tags)]
+    json.dump(to_serialize, fp, cls=ProblemEncoder, sort_keys=True)
+
+
+def serialize_problems_to_file(problems, tags, path_to_file):
+    with open(path_to_file, "w") as file:
+        serialize_problems(problems, tags, file)
