1d99d69aa27e9b546a6cf1b9d325e00d6007e714
Dmitry Sutygin <demonscorpion@yandex.ru>
2015-05-30 11:14:41 +0400

    updated: problem.py

--- problem.py
+++ problem.py
@@ -12,3 +12,16 @@ class Problem:
         self.solutions = [] if solutions is None else solutions
         self.statement = statement
         self.link = link
+
+    def __hash__(self):
+        return hash((self.link, self.statement))
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.link == other.link and self.statement == other.statement
+        return False
+
+    def __ne__(self, other):
+        if isinstance(other, self.__class__):
+            return not self.__eq__(other)
+        return False
