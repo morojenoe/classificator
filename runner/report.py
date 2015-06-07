b24e3dcc4f9f90af3126754ca83b0ee0e0113b6a
Dmitry Sutygin <demonscorpion@yandex.ru>
2015-06-07 17:07:48 +0400

    updated: report.py. Improved report when number of classificators more than one

--- runner/report.py
+++ runner/report.py
@@ -1,19 +1,23 @@
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, label_ranking_average_precision_score
 from sklearn.preprocessing import MultiLabelBinarizer
 
 
 def make_right_order(testing_problems, predicted_problems, predicted_tags):
     problem2id = {problem: i for i, problem in enumerate(testing_problems)}
-    problems, tags = [0]*len(testing_problems), [0]*len(testing_problems)
+    problems, tags = [0] * len(testing_problems), [0] * len(testing_problems)
     for p, t in zip(predicted_problems, predicted_tags):
         problems[problem2id[p]] = p
         tags[problem2id[p]] = t
     return problems, tags
 
 
-def print_report(testing_problems, testing_tags, predicted_problems, predicted_tags):
+def print_report(name_classificator, testing_problems, testing_tags, predicted_problems, predicted_tags):
     predicted_problems, predicted_tags = make_right_order(testing_problems, predicted_problems, predicted_tags)
     mlb = MultiLabelBinarizer().fit(testing_tags + predicted_tags)
     testing_tags = mlb.transform(testing_tags)
     predicted_tags = mlb.transform(predicted_tags)
+    print(name_classificator)
     print(classification_report(testing_tags, predicted_tags, target_names=mlb.classes_))
+    print('label ranking average precision score =',
+          label_ranking_average_precision_score(testing_tags, predicted_tags))
+    print('\n', ('#'*100), '\n')
