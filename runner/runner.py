50fa81d73c222527cc684a1dcd01e87cb1406649
Dmitry Sutygin <demonscorpion@yandex.ru>
2015-07-23 13:58:40 +0400

    updated: split problems into codeforces and timus

--- runner/runner.py
+++ runner/runner.py
@@ -34,11 +34,28 @@ def get_testing_set(path_to_testing, training_problems, training_tags):
     return training_problems, testing_problems, training_tags, testing_tags
 
 
+def get_split():
+    problems, tags = get_training_set_from_db()
+    training_problems, testing_problems, training_tags, testing_tags = [], [], [], []
+
+    for p, t in zip(problems, tags):
+        if 'timus' in p.link:
+            testing_problems.append(p)
+            testing_tags.append(t)
+        else:
+            training_problems.append(p)
+            training_tags.append(t)
+
+    return training_problems, testing_problems, training_tags, testing_tags
+
+
 def run_classification(classificators, path_to_training, path_to_testing):
     training_problems, training_tags = get_training_set(path_to_training)
     training_problems, testing_problems, training_tags, testing_tags = get_testing_set(path_to_testing,
                                                                                        training_problems, training_tags)
     path_to_training, path_to_testing, path_to_result = get_free_files()
+
+    # training_problems, testing_problems, training_tags, testing_tags = get_split()
 
     for classificator in classificators:
         serialize_problems_to_file(training_problems, training_tags, path_to_training)
@@ -54,9 +71,9 @@ def run_classification(classificators, path_to_training, path_to_testing):
             predicted_problems, predicted_tags = deserialize_problems_from_file(path_to_result)
             print_report(classificator, testing_problems, testing_tags, predicted_problems, predicted_tags)
 
-    path_to_result.remove_p()
-    path_to_training.remove_p()
-    path_to_testing.remove_p()
+    # path_to_result.remove_p()
+    # path_to_training.remove_p()
+    # path_to_testing.remove_p()
 
 
 def main():
